6036fbe170b39209f2f93c01db3560ee4ae3ede5
dndhinoja <dev.dipali.dhinoja@gmail.com>
2020-08-04 23:00:52 +0530

    first commit

---
+++ src/test/java/com/nd/SpringFramework51/SpringFramework51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nd.SpringFramework51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework51ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
