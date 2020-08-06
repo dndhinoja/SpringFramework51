af2a0d6f96f6f2692ba7df38ce2b1095d436da5e
dndhinoja <dev.dipali.dhinoja@gmail.com>
2020-08-06 20:16:40 +0530

    Assignment:3 Display List of Authors

---
+++ src/main/java/com/nd/SpringFramework51/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package com.nd.SpringFramework51.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nd.SpringFramework51.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+	private final AuthorRepository authorRepository;
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		super();
+		this.authorRepository = authorRepository;
+	}
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors/list";
+	}
+}
